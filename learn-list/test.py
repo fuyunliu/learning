cd4a67180d4f89a00b398b2a73f769da02cfb085
刘福云 <920507252@qq.com>
2016-09-29 14:41:01 +0800

    ADD

--- learn-list/test.py
+++ learn-list/test.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """收集列表操作方法"""
-
-import pprint
+# import pprint
 
 
 def chunks(l, n):
@@ -11,6 +10,29 @@ def chunks(l, n):
 
 
 # pprint.pprint(list(chunks([{},{},{},{},{},{},{},{},{},{},{},{},{}], 2)))
-
-for i in list(chunks([{},{},{},{},{},{},{},{},{},{},{},{},{}], 2)):
+for i in list(chunks([{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}], 2)):
     print(i)
+
+
+# 对字典进行切片
+def dict_slice(d, start, end):
+    keys = list(d.keys())
+    new_dict = {k: v for k, v in d.items() if k in keys[start:end]}
+    del d, keys
+    return new_dict
+
+
+a = dict_slice({'a': 1, 'b': 2, 'c': 3, 'd': 4}, 2, 3)
+print(a)
+
+
+# 对字典进行排序是不可能的，只能将字典转化为另一种有序的表述，如列表、元组
+import operator
+x = {'a': 1, 'b': 1, 'c': 3, 'd': 4, 'c': 5}
+# 按字典的value进行排序，注意得到的sorted_x是列表
+sorted_x1 = sorted(x.items(), key=operator.itemgetter(1))
+# 按字典的key进行排序，注意得到的sorted_x是列表
+sorted_x2 = sorted(x.items(), key=operator.itemgetter(0))
+# 可以将得到的有序列表转化为OrderedDict，这样得到的字典比一般字典大2倍
+from collections import OrderedDict
+order_dict = OrderedDict(sorted_x1) or OrderedDict(sorted_x2)
