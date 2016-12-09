bccb20a64ac36ead9c6e31555e888be2014e10fd
刘福云 <920507252@qq.com>
2016-12-09 18:10:21 +0800

    学习werkzeug

---
+++ learn-werkzeug/helloworld.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from werkzeug.wrappers import Request, Response
+
+
+@Request.application
+def application(request):
+    return Response('Hello, World!')
+
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple('localhost', 8000, application)
